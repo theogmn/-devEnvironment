3c238998071ec1f843b32d27cf453cfb6c0182e1
Theo Menezes <theom2010@gmail.com>
2020-02-24 10:12:18 -0300

    Correção eslint e prettier

--- react-reactNative/.eslintrc.js
+++ react-reactNative/.eslintrc.js
@@ -1,30 +1,35 @@
 module.exports = {
   env: {
+    browser: true,
     es6: true,
-    node: true,
   },
   extends: [
-    'airbnb-base',
-    'prettier'
-  ],
-  plugins:[
-    'prettier'
+    'airbnb',
+    'prettier',
+    'prettier/react'
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: [
+    'react',
+    'prettier',
+  ],
   rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    "camelcase": "off",
-    "no-unused-vars": [
-      "error", { "argsIgnorePattern": "next" }
-    ]
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js'] }
+    ],
+    'import/prefer-default-export': 'off'
   },
 };
